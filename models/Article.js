4f55539fe464150854255f28b0897ed53b9a5a0e
Rich McRary <rgmcrary@gmail.com>
2017-10-27 16:26:27 -0400

    Create models

--- models/Article.js
+++ models/Article.js
@@ -1,29 +1,42 @@
 // Require mongoose
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 // Create Schema class
 var Schema = mongoose.Schema;
 
 // Create article schema
 var ArticleSchema = new Schema({
-  // title is a required string
+  // Title is a required string
+  post_id: {
+    type: String,
+    required: true
+  },
   title: {
     type: String,
     required: true
   },
-  // link is a required string
+  // Link is a required string
   link: {
     type: String,
     required: true
   },
+  // Summary is a required string
+  summary: {
+    type: String,
+    required: true
+  },
+  saved:{
+    type: Boolean,
+    required: false
+  },
   // This only saves one note's ObjectId, ref refers to the Note model
   note: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: 'Note'
   }
 });
 
 // Create the Article model with the ArticleSchema
-var Article = mongoose.model("Article", ArticleSchema);
+var Article = mongoose.model('Article', ArticleSchema);
 
 // Export the model
 module.exports = Article;
