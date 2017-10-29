fd00ba62b5111627a6c70f610d4e591a9d351d51
Rich McRary <rgmcrary@gmail.com>
2017-10-29 13:19:44 -0400

    adjusted note schema

--- models/Note.js
+++ models/Note.js
@@ -5,18 +5,17 @@ var Schema = mongoose.Schema;
 
 // Create the Note schema
 var NoteSchema = new Schema({
-  // Just a string
-  title: {
-    type: String
+
+  post_id: {
+    type: String,
+    required: true
   },
-  // Just a string
-  body: {
-    type: String
+  noteText: {
+    type: String,
+    required: true
   }
 });
 
-// Remember, Mongoose will automatically save the ObjectIds of the notes
-// These ids are referred to in the Article model
 
 // Create the Note model with the NoteSchema
 var Note = mongoose.model("Note", NoteSchema);
